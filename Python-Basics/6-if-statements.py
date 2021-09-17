449ee8bd1e1212caddb1be7ec9959981bbfc356a
Uchiha Shahin <ashahin762@gmail.com>
2021-09-18 01:12:33 +0600

    If statement

---
+++ Python-Basics/6-if-statements.py
@@ -0,0 +1,13 @@
+# we are starting from if loop of python
+
+is_hot = True
+is_cold= True
+
+if is_hot:
+    print("it's a Hot Day")
+
+elif is_cold:
+    print("It's a cold Day")
+else:
+    print("It's a Lovely day")
+print("Enjoy your day")
