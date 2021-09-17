64e7fffa6e1434334595e9ac91396ef337a51936
Uchiha Shahin <ashahin762@gmail.com>
2021-09-18 00:54:22 +0600

    Python Math Modules

---
+++ Python-Basics/5-math-functions.py
@@ -0,0 +1,12 @@
+import math
+# we can use round to round the value...
+# ?round_value= float(input("input the number: "))
+
+round_value= 3.9
+print(round(round_value))
+
+# abs always returns positive values
+print(abs(-3.9))
+
+#! here you can find more about python math modules..
+# https://docs.python.org/3/library/math.html
