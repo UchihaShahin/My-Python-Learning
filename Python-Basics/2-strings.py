2addaaceb38c32c39537cae55dcf134afc16bf06
Uchiha Shahin <ashahin762@gmail.com>
2021-09-17 16:56:53 +0600

    here I learned about strings

---
+++ Python-Basics/2-strings.py
@@ -0,0 +1,31 @@
+# course = 'Python's Course for Beginners"
+
+# Here we can't use single quote inside the variable so we will just use double quote to highlight the single quote
+
+single_quote = "Python's Course for Beginners"
+print(single_quote)
+
+# ?same for the double quote too, we will use single quote
+
+double_quote = 'python for "beginners"'
+print(double_quote)
+
+#-----------------------------------------------------------
+# ?to use multiple line string we have to add triple quotes
+
+multiple_line_string = '''
+Hello Shahin!
+How are you?
+it's Been many days since I saw you..
+
+'''
+
+print(multiple_line_string)
+
+# -----------------------------
+course ="python for beginners"
+print(course[0])  # it will print the first letter of the string
+
+print(course[-1]) # and this is print the last letter of the string
+
+print(course[0:3]) # !it excludes the 3rd index and print from 0 to 2 index
