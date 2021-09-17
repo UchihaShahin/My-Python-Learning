072787987c2c3cc04a314e0517e7d06caefecb4a
Uchiha Shahin <ashahin762@gmail.com>
2021-09-17 22:16:50 +0600

    len( ), replace( ), find( ), lower( ), upper( )

--- Python-Basics/4-string-methods.py
+++ Python-Basics/4-string-methods.py
@@ -1,1 +1,23 @@
-print ("hello world")
+# !here we will learn about many kinds of string methods.
+
+# To count the length of a string we use len() Function
+
+string = "My name is Shahin"
+
+print(len(string))   #17
+
+print(string.lower())
+print(string.upper())
+
+# !to find a letter we use find()
+
+print(string.find('name'))
+
+# !To replace a sentence or character we use replace()
+
+print(string.replace('name', 'nickname'))
+
+# !to check whether the letter or character is in the string
+
+print('name' in string)
+
