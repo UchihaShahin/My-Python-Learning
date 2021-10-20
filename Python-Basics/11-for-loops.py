3cdf9fbe2246406ab0cb09ef79730132fb94e049
Uchiha Shahin <ashahin762@gmail.com>
2021-10-21 02:13:29 +0600

    Basic for loop

---
+++ Python-Basics/11-for-loops.py
@@ -0,0 +1,24 @@
+# for item in 'python':
+#     print(item)
+
+# for item in [1,2,3,4]:
+#     print(item)
+
+# Print item from 0-9
+# for item in range(0,10):
+#     print(item)
+
+#print item for only even numbers
+# for item in range(1,10,2):
+#     print(item)
+
+#print total of this list using for loops
+#prices [10,20,30]
+
+prices = [10,20,30]
+total=0
+
+for price in prices:
+    total += price
+
+print(total)
