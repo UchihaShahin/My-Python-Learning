1ee36920d9a261f8af9eb95d3f402c3ead4dec3c
Uchiha Shahin <ashahin762@gmail.com>
2021-10-21 01:43:33 +0600

    A basic of what I learned so far, using while loop, conditions etc.

--- Python-Basics/10.2-building-car-game.py
+++ Python-Basics/10.2-building-car-game.py
@@ -5,19 +5,19 @@
 #           "quit - to exit\n")
 
 while (1):
-    user_input=input("What do you want? ")
-    if user_input.upper() == 'HELP':
+    user_input=input("What do you want? ").upper()
+    if user_input == 'HELP':
         print("start - to start the car\n"
               "stop - to stop the car\n"
               "quit - to exit\n")
-    elif user_input.upper() == 'START':
+    elif user_input == 'START':
         print("Car started ready to go\n")
 
-    elif user_input.upper() == 'STOP':
+    elif user_input == 'STOP':
         print("Car Stopped\n")
 
-    elif user_input.upper() == 'QUIT':
+    elif user_input == 'QUIT':
         break
 
     else:
-        print("I cant understand the command")
+        print("I cant understand the command\n")
