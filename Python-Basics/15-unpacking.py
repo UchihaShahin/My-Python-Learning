7220315c16dfd03302cdf564a792e80b77015b2e
Uchiha Shahin <ashahin762@gmail.com>
2021-10-22 14:38:16 +0600

    an operation that consists of assigning an iterable of values to a tuple (or list ) of variables in a single assignment statement

---
+++ Python-Basics/15-unpacking.py
@@ -0,0 +1,11 @@
+# suppose we want to access the values of
+coordinates = (1,2,3)
+#what we would do? we will write something like this
+
+# x= coordinates[0]
+# y= coordinates [1]
+# z= coordinates [2]
+
+# but with unpacking we can do this in less lines
+x,y,z = coordinates
+print(x,y,z)
