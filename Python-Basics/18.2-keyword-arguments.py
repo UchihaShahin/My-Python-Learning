3da788bbf5d7ea5c9b2012825892196e7b7990c8
Uchiha Shahin <ashahin762@gmail.com>
2021-10-28 20:50:24 +0600

    Functions

---
+++ Python-Basics/18.2-keyword-arguments.py
@@ -0,0 +1,10 @@
+def greet_user(first_name, last_name):
+    print(f'Hi, {first_name} {last_name}!')
+
+
+print("Start")
+greet_user(last_name="alam",first_name="shahin")
+print("Finish")
+
+
+#in keyword arguments position doesn't matter
